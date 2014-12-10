7e3f120b4a97ee0a9b53625b4f025d858e8d4ea7
KoSMoS <kosmos123@gmail.com>
2014-12-10 12:34:23 +0200

    fancybox

--- src/Sandbox/WebsiteBundle/Resources/public/frontend/js/travelbase.js
+++ src/Sandbox/WebsiteBundle/Resources/public/frontend/js/travelbase.js
@@ -378,6 +378,22 @@ $(document).ready(function() {
     });
 
 
+    ////////////////////////////////////////////////////////////////////////////////
+    //FANCYBOX
+    $(".image-img > a").fancybox({
+        prevEffect	: 'none',
+        nextEffect	: 'none',
+        helpers	: {
+            title	: {
+                type: 'outside'
+            },
+            thumbs	: {
+                width	: 50,
+                height	: 50
+            }
+        }
+    });
+    /////////////////////////////////////////////////////////////////////////////////
 
 
     $form.find(':input').change(formChange);
