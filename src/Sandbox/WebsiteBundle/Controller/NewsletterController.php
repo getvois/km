f792e9a09efcf705675db14c249b6f85d7aa2e2d
Konstantin Sepp <kosmos123@gmail.com>
2015-02-20 16:27:05 +0200

    newsletter

--- src/Sandbox/WebsiteBundle/Controller/NewsletterController.php
+++ src/Sandbox/WebsiteBundle/Controller/NewsletterController.php
@@ -30,13 +30,14 @@ class NewsletterController extends Controller
             $output .= "Date: ".$headerInfo->date.'<br/>';
             $output .= "From: ".$headerInfo->fromaddress.'<br/>';
             //$output .= "To: ".$headerInfo->reply_toaddress.'<br/>';
-            //$emailStructure = imap_fetchstructure($inbox,$mail);
-            //if(!isset($emailStructure->parts)) {
-                //$output .= "Body: ".imap_body($inbox, $mail, FT_PEEK);
-            //} else {
+            $emailStructure = imap_fetchstructure($inbox,$mail);
+            var_dump($emailStructure);
+            if(!isset($emailStructure->parts)) {
+                $output .= "Body: ".imap_body($inbox, $mail, FT_PEEK);
+            } else {
                 //
                 $output .= imap_qprint("Body: ".imap_body($inbox, $mail, FT_PEEK));
-            //}
+            }
             echo $output;
             $output = '';
 
@@ -50,3 +51,28 @@ class NewsletterController extends Controller
         return new Response("");
     }
 }
+
+/* ALL - return all messages matching the rest of the criteria
+  ANSWERED - match messages with the \\ANSWERED flag set
+  BCC "string" - match messages with "string" in the Bcc: field
+  BEFORE "date" - match messages with Date: before "date"
+  BODY "string" - match messages with "string" in the body of the message
+  CC "string" - match messages with "string" in the Cc: field
+  DELETED - match deleted messages
+  FLAGGED - match messages with the \\FLAGGED (sometimes referred to as Important or Urgent) flag set
+  FROM "string" - match messages with "string" in the From: field
+  KEYWORD "string" - match messages with "string" as a keyword
+  NEW - match new messages
+  OLD - match old messages
+  ON "date" - match messages with Date: matching "date"
+  RECENT - match messages with the \\RECENT flag set
+  SEEN - match messages that have been read (the \\SEEN flag is set)
+  SINCE "date" - match messages with Date: after "date"
+  SUBJECT "string" - match messages with "string" in the Subject:
+  TEXT "string" - match messages with text "string"
+  TO "string" - match messages with "string" in the To:
+  UNANSWERED - match messages that have not been answered
+  UNDELETED - match messages that are not deleted
+  UNFLAGGED - match messages that are not flagged
+  UNKEYWORD "string" - match messages that do not have the keyword "string"
+  UNSEEN - match messages which have not been read yet*/
