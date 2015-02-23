2e2aaf1a5e3641ef72a629789ec87fc42909f595
KoSMoS <kosmos123@gmail.com>
2015-02-23 12:23:13 +0200

    newsletter

--- src/Sandbox/WebsiteBundle/Controller/NewsletterController.php
+++ src/Sandbox/WebsiteBundle/Controller/NewsletterController.php
@@ -48,13 +48,13 @@ class NewsletterController extends Controller
                                 //$delete = str_replace('ä', '&auml;', $delete);
                                 $body = str_replace($delete, '', $body);
 
-//                                $tds = $crawler->filter("td");
-//                                for($j = 2; $j<$tds->count(); $j++){//body in td
-//                                    if(preg_match('/Ei soovi rohkem kirju saada?/', $tds->eq($j)->text())){
-//                                        $delete = $tds->eq($j)->html();
-//                                        $body = str_replace($delete, '', $body);
-//                                    }
-//                                }
+                                $tds = $crawler->filter("td");
+                                for($j = 2; $j<$tds->count(); $j++){//body in td
+                                    if(preg_match('/Ei soovi rohkem kirju saada?/', $tds->eq($j)->text())){
+                                        $delete = $tds->eq($j)->html();
+                                        $body = str_replace($delete, '', $body);
+                                    }
+                                }
                             }
 
                         //tallink
@@ -63,10 +63,10 @@ class NewsletterController extends Controller
                         $patterns = [
                             '/Kui (Sa|Te) ei (soovi|näe)/',
                             '/Kui soovid uudiskirja/',
-                            '/Ei soovi rohkem kirju saada?/',
+                            //'/Ei soovi rohkem kirju saada?/',
                         ];
                         foreach ($patterns as $pattern) {
-                            for($j = 0; $j<$paragraphs->count(); $j++){
+                            for($j = 2; $j<$paragraphs->count(); $j++){
                                 if(preg_match($pattern, $paragraphs->eq($j)->text())
                                 ){
                                     var_dump($paragraphs->eq($j)->children()->count());
