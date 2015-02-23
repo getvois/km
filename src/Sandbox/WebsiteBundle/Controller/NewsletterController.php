8b5aecd7ad0f38310d9333d0682e2e178930f5f0
KoSMoS <kosmos123@gmail.com>
2015-02-23 16:47:18 +0200

    newsletter

--- src/Sandbox/WebsiteBundle/Controller/NewsletterController.php
+++ src/Sandbox/WebsiteBundle/Controller/NewsletterController.php
@@ -38,7 +38,7 @@ class NewsletterController extends Controller
 
             $elements = imap_mime_header_decode($headerInfo->subject);
             var_dump($elements);
-            $subject = utf8_decode($elements[0]->text);
+            $subject = utf8_encode($elements[0]->text);
             $output .= "Subject: ". $subject .'<br/>';
             //$output .= "To: ".$headerInfo->toaddress.'<br/>';
             //$output .= "Date: ".$headerInfo->date.'<br/>';
