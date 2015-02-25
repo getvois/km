483b74d15690c38ff09c983e2b641ebefbdd96f4
KoSMoS <kosmos123@gmail.com>
2015-02-25 13:43:38 +0200

    newsletter

--- src/Sandbox/WebsiteBundle/Controller/NewsletterController.php
+++ src/Sandbox/WebsiteBundle/Controller/NewsletterController.php
@@ -45,6 +45,7 @@ class NewsletterController extends Controller
             if($headerInfo->fromaddress == 'Estonian Air <noreply@estonian-air.ee>') continue;
 
             $elements = imap_mime_header_decode($headerInfo->subject);
+            var_dump($elements);
             $subject = utf8_encode($elements[0]->text);//iconv_mime_decode($elements[0]->text,0,"UTF-8");
             $output .= "Subject: ". $subject .'<br/>';
             //$output .= "To: ".$headerInfo->toaddress.'<br/>';
