9ffd9574ee0d72e4b0f6388ee19f81c9c68069ca
KoSMoS <kosmos123@gmail.com>
2015-03-24 14:47:57 +0200

    cache

--- app/AppCache.php
+++ app/AppCache.php
@@ -6,4 +6,17 @@ use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 class AppCache extends HttpCache
 {
+    protected function getOptions()
+    {
+        return array(
+            'debug'                  => true,
+            'default_ttl'            => 0,
+            'private_headers'        => array('Authorization', 'Cookie'),
+            'allow_reload'           => true,
+            'allow_revalidate'       => true,
+            'stale_while_revalidate' => 1,
+            'stale_if_error'         => 60,
+        );
+    }
+
 }
