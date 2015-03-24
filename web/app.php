48f129497afe2afdcaaac90551fad37adb78de31
KoSMoS <kosmos123@gmail.com>
2015-03-24 14:48:52 +0200

    cache

--- web/app.php
+++ web/app.php
@@ -20,10 +20,10 @@ $environment = str_replace(".", "_", $_SERVER['HTTP_HOST']);
 $kernel = new AppKernel($environment, true);
 
 $kernel->loadClassCache();
-if (!isset($_SERVER['HTTP_SURROGATE_CAPABILITY']) || false === strpos($_SERVER['HTTP_SURROGATE_CAPABILITY'], 'ESI/1.0')) {
-    require_once __DIR__.'/../app/AppCache.php';
-    $kernel = new AppCache($kernel);
-}
+//if (!isset($_SERVER['HTTP_SURROGATE_CAPABILITY']) || false === strpos($_SERVER['HTTP_SURROGATE_CAPABILITY'], 'ESI/1.0')) {
+//    require_once __DIR__.'/../app/AppCache.php';
+//    $kernel = new AppCache($kernel);
+//}
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
