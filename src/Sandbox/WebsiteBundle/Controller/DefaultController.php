0142193bb40586fb28b03718bc7c942936fd23b2
KoSMoS <kosmos123@gmail.com>
2015-03-25 15:00:41 +0200

    mobile form

--- src/Sandbox/WebsiteBundle/Controller/DefaultController.php
+++ src/Sandbox/WebsiteBundle/Controller/DefaultController.php
@@ -111,13 +111,13 @@ class DefaultController extends Controller
                 //$price = $this->get('translator')->trans('price', [], 'frontend');
 
                 $table .= '<div><div class="row table-header sort-controls">';
-                $table .= '<div class="col-md-6">';
+                $table .= '<div class="col-xs-6">';
                 $table .= '    <div class="btn-group" role="group" aria-label="...">
 	                             <div class="btn btn-default ' . (($field=='date' && $fieldOrder=='desc')?"active":"") . '"><a href="#" data-field="date" data-order="asc"><span class="glyphicon glyphicon-arrow-down"></span></a></div>
 	                             <div class="btn btn-default ' . (($field=='date' && $fieldOrder=='asc')?"active":"") . '"><a href="#" data-field="date" data-order="desc"><span class="glyphicon glyphicon-arrow-up"></span></a></div>
 	                           </div>
                            </div>
-                           <div class="col-xs-1 nopadding pull-right">';
+                           <div class="col-xs-2 nopadding pull-right">';
 	            $table .= '    <div class="btn-group" role="group" aria-label="...">
 	                              <div class="btn btn-default ' . (($field=='price' && $fieldOrder=='desc')?"active":"") . '"><a href="#" data-field="price" data-order="asc"><span class="glyphicon glyphicon-arrow-down"></span></a></div>
 	                              <div class="btn btn-default ' . (($field=='price' && $fieldOrder=='asc')?"active":"") . '"><a href="#" data-field="price" data-order="desc"><span class="glyphicon glyphicon-arrow-up"></span></a></div>
