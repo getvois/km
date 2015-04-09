0d6ae1118dae363d1ad2c6cbada09db6acc1dcf5
KoSMoS <kosmos123@gmail.com>
2015-04-09 16:28:23 +0300

    offer route and template

--- src/Sandbox/WebsiteBundle/Controller/OfferController.php
+++ src/Sandbox/WebsiteBundle/Controller/OfferController.php
@@ -4,8 +4,10 @@ namespace Sandbox\WebsiteBundle\Controller;
 
 use Pagerfanta\Adapter\ArrayAdapter;
 use Pagerfanta\Pagerfanta;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 class OfferController extends Controller
@@ -34,7 +36,7 @@ class OfferController extends Controller
         $data = json_decode($content);
 
         $countries = [];
-        foreach ($data as &$offer) {
+        foreach ($data->offers as &$offer) {
             if($offer->country){
                 $field = 'name_'.$lang;
                 $offer->country->name = $offer->country->$field;
@@ -57,5 +59,70 @@ class OfferController extends Controller
             'offerTypes' => $offerTypes,
             'countries' => $countries,
         ];
+    }
+
+
+    /**
+     * @Route("/offers.get/")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function getOffersAction(Request $request)
+    {
+        $query = $this->getQuery($request);
+
+        $content = @file_get_contents('http://api.travelwebpartner.com/app_dev.php/api/offer.filter/?'.$query);
+        if(!$content)
+            return new JsonResponse(['html' => '']);
+
+        $data = json_decode($content);
+
+        $html = $this->get('templating')->render('@SandboxWebsite/Offer/fromJson.html.twig', ['data' => $data]);
+
+        return new JsonResponse(['html' => $html]);
+    }
+
+    private function getQuery(Request $request)
+    {
+        $query = [];
+        //type //int or name;
+        if($request->query->has('type')){
+            $query[] = 'type='.$request->query->get('type');
+        }
+
+        //city //int or name, coma separated list
+        if($request->query->has('city')){
+            $query[] = 'city='.$request->query->get('city');
+        }
+
+        //company //int or name
+        if($request->query->has('company')){
+            $query[] = 'company='.$request->query->get('company');
+        }
+
+        //withFlights//1 or 0
+        if($request->query->has('withFlights')){
+            $query[] = 'withFlights='.$request->query->get('withFlights');
+        }
+
+        //price//priceFrom & priceTo
+        if($request->query->has('priceFrom')){
+            $query[] = 'priceFrom='.$request->query->get('priceFrom');
+        }
+        if($request->query->has('priceTo')){
+            $query[] = 'priceTo='.$request->query->get('priceTo');
+        }
+
+        //limit//int
+        if($request->query->has('limit')){
+            $query[] = 'limit='.$request->query->get('limit');
+        }
+        //offset//int
+        if($request->query->has('offset')){
+            $query[] = 'offset='.$request->query->get('offset');
+        }
+
+        $query = implode("&", $query);
+        return $query;
     }
 }
