57519dbcdc5b4403d0bb8a9aa75e8996999ef02e
KoSMoS <kosmos123@gmail.com>
2015-05-18 15:17:24 +0300

    change parent

---
+++ src/Sandbox/WebsiteBundle/Command/ChangeParentCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Sandbox\WebsiteBundle\Command;
+
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ChangeParentCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('travelbase:kuma:change-node-parent')
+            ->setDescription('Change the parent of a Node')
+            ->addArgument(
+                'child',
+                InputArgument::REQUIRED,
+                'ID of child not that will be moved'
+            )
+            ->addArgument(
+                'parent',
+                InputArgument::REQUIRED,
+                'ID of parent Node to move child to'
+            )
+        ;
+    }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em=$this->getContainer()->get('doctrine')->getManager();
+        $child=$em->getRepository('KunstmaanNodeBundle:Node')->find($input->getArgument('child'));
+        $parent=$em->getRepository('KunstmaanNodeBundle:Node')->find($input->getArgument('parent'));
+        $child->setParent($parent);
+        $em->persist($child);
+        $em->flush();
+        $output->writeln('Done.');
+    }
+}
