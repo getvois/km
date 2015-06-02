c2a423791d8ee169b5858c32c3d21e9f3962ea09
KoSMoS <kosmos123@gmail.com>
2015-06-02 13:46:50 +0300

    map category

---
+++ src/Sandbox/WebsiteBundle/AdminList/MapCategoryAdminListConfigurator.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Sandbox\WebsiteBundle\AdminList;
+
+use Doctrine\ORM\EntityManager;
+
+use Sandbox\WebsiteBundle\Form\MapCategoryAdminType;
+use Kunstmaan\AdminListBundle\AdminList\FilterType\ORM;
+use Kunstmaan\AdminListBundle\AdminList\Configurator\AbstractDoctrineORMAdminListConfigurator;
+use Kunstmaan\AdminBundle\Helper\Security\Acl\AclHelper;
+
+/**
+ * The admin list configurator for MapCategory
+ */
+class MapCategoryAdminListConfigurator extends AbstractDoctrineORMAdminListConfigurator
+{
+    /**
+     * @param EntityManager $em        The entity manager
+     * @param AclHelper     $aclHelper The acl helper
+     */
+    public function __construct(EntityManager $em, AclHelper $aclHelper = null)
+    {
+        parent::__construct($em, $aclHelper);
+        $this->setAdminType(new MapCategoryAdminType());
+    }
+
+    /**
+     * Configure the visible columns
+     */
+    public function buildFields()
+    {
+        $this->addField('name', 'Name', true);
+    }
+
+    /**
+     * Build filters for admin list
+     */
+    public function buildFilters()
+    {
+        $this->addFilter('name', new ORM\StringFilterType('name'), 'Name');
+    }
+
+    /**
+     * Get bundle name
+     *
+     * @return string
+     */
+    public function getBundleName()
+    {
+        return 'SandboxWebsiteBundle';
+    }
+
+    /**
+     * Get entity name
+     *
+     * @return string
+     */
+    public function getEntityName()
+    {
+        return 'MapCategory';
+    }
+}
