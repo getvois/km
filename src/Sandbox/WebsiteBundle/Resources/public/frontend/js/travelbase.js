ad3aa10267c29fc885f6f7c333931b01f1e0cfa0
KoSMoS <kosmos123@gmail.com>
2015-06-19 16:08:55 +0300

    form

--- src/Sandbox/WebsiteBundle/Resources/public/frontend/js/travelbase.js
+++ src/Sandbox/WebsiteBundle/Resources/public/frontend/js/travelbase.js
@@ -942,7 +942,7 @@ $(document).ready(function() {
         max: 1000,
         values: [ 0, 700 ],
         slide: function( event, ui ) {
-            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+            $( "#amount" ).val( "€" + ui.values[ 0 ] + " - €" + ui.values[ 1 ] );
         }
     });
     $( "#amount" ).val( $slider.slider( "values", 0 ) + "€ - " + $slider.slider( "values", 1 ) + "€");
