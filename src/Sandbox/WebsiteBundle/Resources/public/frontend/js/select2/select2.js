a84d85781279b2a5aff3a18a09169627efa1bd0c
KoSMoS <kosmos123@gmail.com>
2015-06-19 15:27:08 +0300

    form

--- src/Sandbox/WebsiteBundle/Resources/public/frontend/js/select2/select2.js
+++ src/Sandbox/WebsiteBundle/Resources/public/frontend/js/select2/select2.js
@@ -1451,12 +1451,7 @@ the specific language governing permissions and limitations under the Apache Lic
             mask.show();
 
             this.positionDropdown();
-            console.log($('.select2-result'));
-            console.log($('.select2-result').length);
-            console.log(this.dropdown);
-            console.log(this.dropdown.find('.select2-results'));
-            //if(this.dropdown.find('.select2-results').children().length > 0)
-                this.dropdown.show();
+            this.dropdown.show();
             this.positionDropdown();
 
             this.dropdown.addClass("select2-drop-active");
